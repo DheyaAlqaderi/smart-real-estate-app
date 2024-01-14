7076f4b26b098e9cf632aefd88792e4663489efb
DheyaAlqaderi <116699614+DheyaAlqaderi@users.noreply.github.com>
2024-01-14 05:12:37 +0300

    seventh commit with login API clean code

--- lib/src/ui/screens/homePage.dart
+++ lib/src/ui/screens/homePage.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:smart_real_estate_app/src/services/localdb/Managedb.dart';
 
 class homePage extends StatefulWidget {
   const homePage({super.key});
@@ -12,6 +13,25 @@ class _homePageState extends State<homePage> {
   Widget build(BuildContext context) {
     return  Scaffold(
       backgroundColor: Colors.amber[200],
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          FutureBuilder<String?>(
+            future: Managedb.getToken(),
+            builder: (context, snapshot) {
+              if (snapshot.connectionState == ConnectionState.waiting) {
+                return Center(child: CircularProgressIndicator());
+              } else if (snapshot.hasError) {
+                return Center(child: Text('Error: ${snapshot.error}'));
+              } else {
+                final String? retrievedToken = snapshot.data;
+                return Center(child: Text('Your Token ياغالي  :\n ${retrievedToken ?? "No token available"}'));
+              }
+            },
+          ),
+        ],
+      ),
     );
   }
 }
